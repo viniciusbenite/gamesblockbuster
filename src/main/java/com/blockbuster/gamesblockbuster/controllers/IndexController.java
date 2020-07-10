81effc346a42af9cdde7e35794722d8e544a8874
Vinícius Ribeiro <viniciusribeiro@ua.pt>
2020-07-10 22:12:06 +0100

    Auth0 - broken

---
+++ src/main/java/com/blockbuster/gamesblockbuster/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package com.blockbuster.gamesblockbuster.controllers;
+
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String home(Model model, @AuthenticationPrincipal OidcUser principal) {
+        return "index";
+    }
+}
